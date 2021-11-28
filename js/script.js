2da9a9ab90d6d75bfeb0e6d8fd0fc41596d9fcd6
Oleksandr Shysh <72020976+Shysh-Oleksandr@users.noreply.github.com>
2021-11-28 12:28:34 +0200

    Initial commit.

---
+++ js/script.js
@@ -0,0 +1,127 @@
+// Webp converter
+function testWebP(callback) {
+	var webP = new Image();
+	webP.onload = webP.onerror = function () {
+		callback(webP.height == 2);
+	};
+	webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+
+testWebP(function (support) {
+	if (support == true) {
+		document.querySelector('body').classList.add('webp');
+	}
+	else{
+		document.querySelector('body').classList.add('no-webp');
+	}
+});
+// /Webp converter
+
+// Burger menu
+$(document).ready(function() {
+	$('.icon-menu').click(function(event) {
+		$('.icon-menu,.menu__body').toggleClass('_active');
+		$('body').toggleClass('_lock');
+	});
+});
+// /Burger menu
+
+$(function() {
+	var header = $("#header"),
+		introH = $("#intro").innerHeight(),
+		scrollOffset = $(window).scrollTop();
+
+	/* Fixed Header */
+	checkScroll(scrollOffset);
+
+	$(window).on("scroll", function(){
+		scrollOffset = $(this).scrollTop();
+		checkScroll(scrollOffset);
+	});
+
+	function checkScroll (scrollOffset) {
+		if (scrollOffset >= introH) {
+			header.addClass("fixed");	
+		}
+		else {
+			header.removeClass("fixed");
+		}
+	}
+
+
+	/* Slider */
+	$("[data-slider]").slick({
+		infinity: true,
+		slidesToShow: 1,
+  		slidesToScroll: 1,
+  		fade: false,
+	});
+});
+
+// Scroll to section
+$(function() {
+	$("[data-scroll]").on("click", function(event) {
+		event.preventDefault();
+
+		var $this = $(this),
+			blockId = $this.data('scroll'),
+			blockOffset;
+		if(blockId == "#header"){
+			blockOffset = 0;
+		}
+		else {
+			blockOffset = $(blockId).offset().top;
+		}
+
+		$(".header-bottom__menu a").removeClass("_active");
+		$this.addClass("_active");
+
+		if($("#nav").hasClass("_active")) {
+			$("#nav_toggle").toggleClass("_active");
+			$("#nav").toggleClass("_active");
+		}
+
+		$('body').removeClass("_lock");
+
+		$("html, body").animate({
+			scrollTop: blockOffset
+		}, 500);
+	});
+});
+// /Scroll to section.
+
+// ==========================================================================
+// Scroll active links.
+// Cache selectors
+var topMenu = $(".menu"),
+    topMenuHeight = topMenu.innerHeight() + 15,
+    // All list items
+    menuItems = $('.menu__body a'),
+    // Anchors corresponding to menu items
+    scrollItems = menuItems.map(function(){
+      var item = $($(this).data("scroll"));
+      if (item.length) { return item; }
+    });
+
+
+// Bind to scroll
+$(window).scroll(function(){
+   // Get container scroll position
+   var fromTop = $(this).scrollTop()+topMenuHeight;
+
+   // Get id of current scroll item
+   var cur = scrollItems.map(function(){
+     if ($(this).offset().top < fromTop)
+       return this;
+   });
+   // Get the id of the current element
+   cur = cur[cur.length-1];
+   var id = cur && cur.length ? cur[0].id : "";
+   // Set/remove active class
+   menuItems
+   .removeClass("active")
+     .filter(function(){
+	        return $(this).attr('data-scroll') == "#"+id;
+	    }).addClass("active");
+});
+// /Scroll active links
